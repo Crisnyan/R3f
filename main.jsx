ab8d20dde5c02936216da7be5a17552e63dd1738
Crisnyan <137719640+Crisnyan@users.noreply.github.com>
2026-02-16 08:04:34 +0000

    basic mapping and parsing

--- main.jsx
+++ main.jsx
@@ -1,44 +1,89 @@
 import { useState, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { createRoot } from 'react-dom/client'
+import { MapControls, OrthographicCamera } from "@react-three/drei"
+import { simpleMap, testMap, emptyMap } from './maps'
 
-function Block(props) {
-  const mref = useRef()
+const s = 0.95
+
+function Obstacle(props) {
+  const oRef = useRef()
   const [active, setActive] = 
   useState(false)
-  const [hovered, setHover] = 
-  useState(false)
-  useFrame((state, delta) => 
-    (mref.current.rotation.z += delta))
   return (
   <mesh 
     {...props}
-    ref = {mref}
-    scale = {active ? 1.5 : 1}
+    ref = {oRef}
     onClick = {(event) => 
-      setActive(!active)}
-    onPointerOver = {(event) => 
-      setHover(true)}
-    onPointerOut = {(event) => 
-      setHover(false)}>
-    <boxGeometry args={[2,2,2]}/>
-    <meshStandardMaterial color = 
-    {hovered ? 'orange' : 'green'}/>
+      setActive(!active)}>
+    <boxGeometry 
+    args={[s,s,s]}/>
+    <meshStandardMaterial 
+    color={active ? 'hotpink' : 'orange'}
+    wireframe={false}/>
     </mesh>
   )
 }
 
+function Player(props) {
+  const pRef = useRef()
+  const [selected, setSelected] = useState(false)
+  return (
+    <mesh
+    {...props}
+    ref={pRef}
+    onClick = {(event) =>
+      setSelected(!selected)}>
+    <boxGeometry args={[s,s,s]}/>
+    <meshStandardMaterial
+    color={selected ? 'blue' : 'green'}/>
+    </mesh>
+  )
+}
+
+function Floor() {
+  return (
+  <mesh
+    position={[-0.5, 0,-0.5]}
+    rotation={[-Math.PI/2, 0,0]}
+    receiveShadow>
+    <planeGeometry args={[10,10]}/>
+    <meshStandardMaterial/>
+  </mesh>
+  )
+}
+
+function Scene(props) {
+  const map = testMap
+  const [entities, setEntities] = useState(() =>
+  parseMap(map))
+  return (
+  <>
+    <ambientLight intensity={Math.PI/4}/>
+    <pointLight position={[7,6,7]} 
+    decay={0} intensity={2.5} />
+    <OrthographicCamera makeDefault
+    position={[5,5,5]}
+    zoom={25}
+    near={-50}
+    far={100} />
+    <MapControls makeDefault
+    target={[0,0,0]}/>
+    {obstacles}
+    {players}
+    <Floor/>
+  </>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <Canvas>
-    <ambientLight intensity={1/2}/>
-    <spotLight position={[10, 10, 10]} 
-    angle={0.15} penumbra={1} decay={0} 
-    intensity={Math.PI} />
-
-    <pointLight position={[-10, -10, -10]} 
-    decay={0} intensity={Math.PI} />
-
-    <Block position={[-1.2, 0, 0]} />
-    <Block position={[1.2, 0, 0]} />
-  </Canvas>
+  <div style={{
+    width: '390px',
+    height: '820px',
+    background: 'black'
+  }}>
+    <Canvas>
+      <Scene/>
+    </Canvas>
+  </div>
 )
---
+++ maps.js
@@ -0,0 +1,106 @@
+import js from "@eslint/js"
+import { SimplexNoise } from "three/examples/jsm/Addons.js"
+import { depth, roughness } from "three/tsl"
+
+export const testMap = `
+wwwwwwwwww
+wwowwwwoww
+wooowwooow
+ooooooooow
+oooooooooo
+oooooooooo
+ooooooooow
+woooooooow
+wwooooooww
+wwwoooowww,
+
+wwwwwwwwww
+owoowwoowo
+oooooooooo
+oooooooooo
+oooooooooo
+oooooooooo
+oooooooooo
+oooooooooo
+owoooooowo
+wwooooooww,
+
+wwwwwwwwww
+wwooooooww
+woooooooow
+woooooooow
+woooooooow
+wwwwwwwwww
+owwoooowwo
+owwoooowwo                           
+owwwwwwwwo
+oooooooooo
+`
+
+export const simpleMap = `
+wwwoooooww
+ooooowoooo
+oooooooooo
+oooowwooow
+ooowwwwooo
+ooowwwwoow
+oooowwoooo
+woooooooow
+oooooooooo
+oooooooooo
+`
+
+export const emptyMap = ``
+
+export const parseMap = (map) => {
+  const Entities = []
+  const staticTiles = []
+  let mapWidth = 0 //x
+  let mapHeight = 0 //y
+  let mapDepth = 0 //z
+  const levels = map.trim().split(',')
+  mapHeight = levels.length
+  levels.forEach((level, y) => {
+    const rows = level.trim().split('\n')
+    mapDepth = rows.length
+    rows.forEach((row, z) => {
+      const chars = row.trim().split('')
+      mapWidth = chars.length
+      chars.forEach((char, x) => {
+        const id = `${x}-${y}-${z}`
+        const position = {x, y, z}
+        switch (char) {
+          case 'w':
+            staticTiles.push({
+              id,
+              type: 'wall',
+              position
+            })
+            break
+          case 'p':
+            Entities.push({
+              id,
+              type: 'player',
+              position
+            })
+            break
+         case 'e':
+            Entities.push({
+              id,
+              type: 'enemy'
+            })
+            break
+          }
+        })
+      })
+    })  
+  return {
+    staticTiles,
+    Entities,
+    mapInfo : {
+      width: mapWidth,
+      height: mapLength,
+      depth: mapDepth
+    }
+  }       
+}
